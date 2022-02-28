d0bb1bc7802d01180dd4b6923f1154133a3e7a87
yjotyrrm <snowstorm025@gmail.com>
2022-02-28 15:50:09 -0700

    fixed connection string

--- DataStore/Plugins/Dsn.cs
+++ DataStore/Plugins/Dsn.cs
@@ -10,6 +10,6 @@ namespace BlabberApp.DataStore.Plugins
     //just a way to store the DSN in one place so it is easier to change it later if we need to
     public class Dsn
     {
-        public const string DSN = "server=143.110.159.170;uid=rontene;pwd=letmein;database=donstringham";
+        public const string DSN = "server=143.110.159.170;uid=rontene;pwd=letmein;database=rontene";
     }
 }
